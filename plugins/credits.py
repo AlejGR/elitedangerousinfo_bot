8d53b3a357f933026ff0437be5b0e4419ecc075d
Alejandro García <garfil1234@gmail.com>
2016-11-20 00:07:30 +0100

    add config naves,commands in responses and credits

--- plugins/credits.py
+++ plugins/credits.py
@@ -5,4 +5,4 @@ from config import *
 bot.message_handler(commands=['creditos'])
 def commands_credit(m):
     cid = m.chat.id
-    bot.send_message(cid,"Quiero dar las Gracias a la comunidad de Elite Dangerous ESP y a los administrados de esta. \n En especial a @tunotemetas por redactar los textos a @truenox por darme vacacciones pagadas y @edurolp por ayudarme con el bot \n \n CMDR Garfieldrockero ")
+    bot.send_message(cid,"Quiero dar las Gracias a la comunidad de Elite Dangerous ESP y a los administrados de esta. \n En especial a @tunotemetas por redactar los textos,a @cmdt_txus por ceder las configuraciones de Coriolis, a @truenox por darme vacacciones pagadas y @edurolp por ayudarme con el bot \n \n CMDR Garfieldrockero ")
