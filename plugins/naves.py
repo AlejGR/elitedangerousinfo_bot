204d126c3763a3952f2eef81149ed42b3b6db586
Alejandro García <garfil1234@gmail.com>
2016-11-20 22:33:23 +0100

    add ships

--- plugins/naves.py
+++ plugins/naves.py
@@ -12,21 +12,24 @@ def command_naves(m):
     item2 = types.KeyboardButton('Anaconda')
     item3 = types.KeyboardButton('Asp Explorer')
     item4 = types.KeyboardButton('Cobra MkIII')
-    item5 = types.KeyboardButton('Diamondback Explorer')
-    item6 = types.KeyboardButton('Diamondback Scout')
-    item7 = types.KeyboardButton('Eagle')
-    item8 = types.KeyboardButton('Fer-De-Lance')
-    item9 = types.KeyboardButton('Hauler')
-    item10 = types.KeyboardButton('Orca')
-    item11 = types.KeyboardButton('Python')
-    item12 = types.KeyboardButton('Sidewinder')
-    item13 = types.KeyboardButton('Type 6')
-    item14 = types.KeyboardButton('Type 7')
-    item15 = types.KeyboardButton('Type 9')
-    item16 = types.KeyboardButton('Viper MkIII')
-    item17 = types.KeyboardButton('Vulture')
-    item18 = types.KeyboardButton('Inicio')
-    markup.add(item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18)
+    item5 = types.KeyboardButton('Cobra MkIV')
+    item6 = types.KeyboardButton('Diamondback Explorer')
+    item7 = types.KeyboardButton('Diamondback Scout')
+    item8 = types.KeyboardButton('Eagle')
+    item9 = types.KeyboardButton('Fer-De-Lance')
+    item10 = types.KeyboardButton('Hauler')
+    item11 = types.KeyboardButton('Keelback')
+    item12 = types.KeyboardButton('Orca')
+    item13 = types.KeyboardButton('Python')
+    item14 = types.KeyboardButton('Sidewinder')
+    item15 = types.KeyboardButton('Type 6')
+    item16 = types.KeyboardButton('Type 7')
+    item17 = types.KeyboardButton('Type 9')
+    item18 = types.KeyboardButton('Viper MkIII')
+    item19 = types.KeyboardButton('Viper MkIV')
+    item20 = types.KeyboardButton('Vulture')
+    item21 = types.KeyboardButton('Inicio')
+    markup.add(item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20, item21)
     bot.send_message(m.chat.id, "Selecione una opción: ", reply_markup=markup)
     userStep[m.chat.first_name] = 'comandante'
 
@@ -35,7 +38,7 @@ def stepnave(m):
     cid = m.chat.id
     msg = m.text[:20]
     msgmin = msg.lower()
-    naves = ['adder', 'anaconda', 'asp explorer','cobra mkiii', 'diamondback explorer', 'diamondback scout', 'eagle', 'fer-de-lance', 'hauler', 'orca', 'python', 'sidewinder', 'type 6', 'type 7', 'type 9', 'viper mkiii', 'vulture']
+    naves = ['adder', 'anaconda', 'asp explorer','cobra mkiii', 'cobra mkiv', 'diamondback explorer', 'diamondback scout', 'eagle', 'fer-de-lance', 'keelback', 'hauler', 'orca', 'python', 'sidewinder', 'type 6', 'type 7', 'type 9', 'viper mkiii', 'viper mkiv', 'vulture']
     if msgmin == "inicio":
         userStep[m.from_user.first_name] = 0
     else:
