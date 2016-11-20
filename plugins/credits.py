4308092f8eeb91a50590a6837fd9ffea70d545bc
Alejandro García <garfil1234@gmail.com>
2016-11-20 02:06:08 +0100

    fix errors

--- plugins/credits.py
+++ plugins/credits.py
@@ -2,7 +2,7 @@
 from config import *
 
 @bot.message_handler(func=lambda m: m.content_type == 'text' and m.text in ['Creditos'])
-bot.message_handler(commands=['creditos'])
+@bot.message_handler(commands=['creditos'])
 def commands_credit(m):
     cid = m.chat.id
     bot.send_message(cid,"Quiero dar las Gracias a la comunidad de Elite Dangerous ESP y a los administrados de esta. \n En especial a @tunotemetas por redactar los textos,a @cmdt_txus por ceder las configuraciones de Coriolis, a @truenox por darme vacacciones pagadas y @edurolp por ayudarme con el bot \n \n CMDR Garfieldrockero ")
