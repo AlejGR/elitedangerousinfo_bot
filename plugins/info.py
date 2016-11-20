3e24dbbaf3be08afc7d802be61f25731b54559c8
Alejandro García <garfil1234@gmail.com>
2016-11-20 14:12:08 +0100

    modify

--- plugins/info.py
+++ plugins/info.py
@@ -9,4 +9,4 @@ def command_info(m):
     item1 = types.InlineKeyboardButton("Grupo Telegram", url="https://telegram.me/EliteESP")
     item2 = types.InlineKeyboardButton("PayPal", url="https://paypal.me/garfieldrockero")
     markup.add(item1,item2)
-    bot.send_message(cid,"@elitedangerous_bot Dispone de una base de datos con los sistemas del juego Elite Dangerous y de sus productos, también cuenta con una recopilación de la información de las naves y radios de la comunidad. \n\n El bot ha sido creado para la comunidad de Elite Dangerous. \n\n @elitedangerous_bot no tienen relación con Elite Dangerous o Frontier Developments.",reply_markup=markup)
+    bot.send_message(cid,"@elitedangerous_bot Dispone de una base de datos con los sistemas del juego Elite Dangerous y de sus productos, también cuenta con una recopilación de la información de las naves y radios de la comunidad. \n\n El bot ha sido creado para la comunidad de Elite Dangerous. \n\n @elitedangerous_bot no tienen relación con Frontier Developments.",reply_markup=markup)
